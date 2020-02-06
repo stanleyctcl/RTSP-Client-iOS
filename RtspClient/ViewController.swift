1798d47726c460750ccedccbfd3f34de39f27da7
Stanley Huang <stanley@ctcl.com.tw>
2020-02-06 13:22:01 +0800

    Update rtsp server address and added UI
    1. Update rtsp server address
    2. Add start and stop button to start/stop rtsp streaming
    3. add busy indicator when connect rtsp server

--- RtspClient/ViewController.swift
+++ RtspClient/ViewController.swift
@@ -9,36 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var busy: UIActivityIndicatorView!
+    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     var video: RTSPPlayer!
-    
-    
+    let urlForTesting = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
+    let urlString = "rtsp://114.32.99.223/v01"
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        video = RTSPPlayer(video: "rtsp://admin:admin@192.168.150.243", usesTcp: false)
-        video.outputWidth = Int32(UIScreen.main.bounds.width)
-        video.outputHeight = Int32(UIScreen.main.bounds.height)
-        video.seekTime(0.0)
-        
-      let timer = Timer.scheduledTimer(timeInterval: 1.0/30, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
-        timer.fire()
+
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.darkGray.cgColor
+        button.layer.cornerRadius = 15
+
+        busy.transform = CGAffineTransform.init(scaleX: 3, y: 3)
     }
-    
-  @objc func update(timer: Timer) {
+
+    @objc func update(timer: Timer) {
         if(!video.stepFrame()){
             timer.invalidate()
             video.closeAudio()
         }
-        imageView.image = video.currentImage
+
+        DispatchQueue.main.async {
+            self.imageView.image = self.video.currentImage
+
+            if self.busy.isAnimating {
+                self.busy.stopAnimating()
+            }
+        }
     }
 
+    @IBAction func onPress(_ sender: UIButton) {
+        if sender.title(for: .normal) == "Start" {
+            sender.setTitle("Exit", for: .normal)
+
+            busy.startAnimating()
+            DispatchQueue(label: "rtspThread").async {
+                self.startStreaming()
+            }
+        } else {
+            sender.setTitle("Start", for: .normal)
+            stopStreaming()
+        }
+    }
+
+    fileprivate func startStreaming() {
+        video = RTSPPlayer(video: urlString, usesTcp: true)
+        video.outputWidth = Int32(1280)
+        video.outputHeight = Int32(720)
+        video.seekTime(0.0)
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0/30, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        timer?.fire()
+    }
+
+    fileprivate func stopStreaming(){
+        timer?.invalidate()
+        video.closeAudio()
+        video = nil
+        imageView.image = nil
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
